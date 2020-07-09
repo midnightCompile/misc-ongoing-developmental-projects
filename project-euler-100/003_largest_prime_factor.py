1d10b416946964f93be35ac9199c5b2741db5353
TheMidnightCompiler <themidnightcompiler@gmail.com>
2020-07-08 23:25:57 -0500

    Created source file to begin developing solution code.

---
+++ project-euler-100/003_largest_prime_factor.py
@@ -0,0 +1,6 @@
+# 003 - Largest Prime Factor
+#
+# Problem: What is the largest prime factor of the number 600851475143?
+#
+# For additional info regarding solution, see 003_largest_prime_factor_notes.md
+
