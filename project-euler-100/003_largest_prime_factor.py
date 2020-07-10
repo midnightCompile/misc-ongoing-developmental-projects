ed4683599ea87ed704942495dcb2d0dd2fd20382
TheMidnightCompiler <themidnightcompiler@gmail.com>
2020-07-09 23:58:50 -0500

    Corrected 002 notes filename, created 003 notes file, cleaned up some code in 003 script .py file

--- project-euler-100/003_largest_prime_factor.py
+++ project-euler-100/003_largest_prime_factor.py
@@ -10,10 +10,10 @@ def prime_factors(n):
     factors = []
     d = 2
     while n > 1:
-        while n % d ==0:
+        while n % d == 0:
             factors.append(d)
             n /= d
-        d = d + 1
+        d += 1
         if d*d > n:
             if n > 1: factors.append(n)
             break
