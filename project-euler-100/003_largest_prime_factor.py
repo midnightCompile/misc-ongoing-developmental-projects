aaea7dc04c76cde1d6b16dce89dc759f0aad9128
TheMidnightCompiler <themidnightcompiler@gmail.com>
2020-07-09 20:54:22 -0500

    Completed algorithm for determining largest prime factor of a number. Script requests user input for positive integer and provides largest prime factor.

--- project-euler-100/003_largest_prime_factor.py
+++ project-euler-100/003_largest_prime_factor.py
@@ -4,3 +4,30 @@
 #
 # For additional info regarding solution, see 003_largest_prime_factor_notes.md
 
+
+# Function to find all primes for a positive integer
+def prime_factors(n):
+    factors = []
+    d = 2
+    while n > 1:
+        while n % d ==0:
+            factors.append(d)
+            n /= d
+        d = d + 1
+        if d*d > n:
+            if n > 1: factors.append(n)
+            break
+    return factors
+
+
+
+# Main function prompting user to provide positive integer to receive largest prime
+def main():
+    num = int(input("Provide a positive integer to determine its largest prime factor: "))
+    primes = prime_factors(num)
+    largest_prime = max(primes)
+    print("The largest prime factor of %s is the number %s" % (num, largest_prime))
+
+if __name__ == "__main__":
+    main()
+
